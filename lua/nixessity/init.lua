92b030e86f95b4a995ca56b585c0cb3496c34170
brandishcode <brandishcode@gmail.com>
2025-06-15 15:19:28 +0900

    Nixhelp: instead of just `print` on the nix <cmd> --help document, display the help doc in a scratch readonly buffer

--- lua/nixessity/init.lua
+++ lua/nixessity/init.lua
@@ -1,12 +1,16 @@
 local NixHelp = require 'nixessity.nix.help'
+local BufferApi = require 'nixessity.buffer-api'
+local Log = require 'nixessity.log'
 
 local Nixessity = {}
 
 function Nixessity.setup(opts)
   opts = opts or {}
   vim.api.nvim_create_user_command('Nixhelp', function(args)
-    local doc = NixHelp:run(args.fargs[1])
-    print(table.concat(doc, '\n'))
+    local cmd = args.fargs[1]
+    Log.debug('Nixhelp ' .. cmd)
+    local doc = NixHelp:run(cmd)
+    BufferApi:docOpen('Nixhelp ' .. cmd .. ' [HELP]', doc)
   end, { desc = 'nix {targetcmd} --help', nargs = 1 })
 end
 
--- lua/nixessity/nix/help.lua
+++ lua/nixessity/nix/help.lua
@@ -1,27 +1,27 @@
 local Job = require 'plenary.job'
-local log = require 'plenary.log':new()
-log.level = 'debug'
 local NixHelp = {}
 
 --Print the 'nix {targetCmd} --help' documentation
 --@param targetCmd string: The nix flake command to print documentation
 --@return string
 function NixHelp:run(targetCmd)
-  local doc
-  Job:new({
+  local stderrResult = ''
+  local nixCmd = Job:new({
     command = 'nix',
     args = { targetCmd, '--help' },
-    on_stdout = function(error, _)
-      if error then
-        log.debug('Nixhelp ' .. targetCmd .. ' error: ' .. error)
+    enable_recording = true,
+    on_exit = function(job)
+      if #job:stderr_result() > 0 then
+        stderrResult = table.concat(job:stderr_result(), '\n')
       end
     end,
-    on_exit = function(job, return_val)
-      log.debug('Nixhelp ' .. targetCmd .. ' return_val: ' .. return_val)
-      doc = job:result()
-    end,
-  }):sync()
-  return doc
+  })
+  local res, code = nixCmd:sync()
+  if code == 0 then
+    return res
+  else
+    error(stderrResult)
+  end
 end
 
 return NixHelp
