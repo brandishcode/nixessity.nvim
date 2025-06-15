87d4551d21579780cc15385963ae6499b4e9e2e9
brandishcode <brandishcode@gmail.com>
2025-06-15 16:41:19 +0900

    Nixhelp: rename private field and opendoc api

--- lua/nixessity/buffer-api.lua
+++ lua/nixessity/buffer-api.lua
@@ -2,11 +2,11 @@ local Log = require 'nixessity.log'
 
 local BufferApi = {}
 
-BufferApi.docBuf = {}
+BufferApi.__docbuf = {}
 
 vim.api.nvim_create_autocmd('BufLeave', {
   callback = function(events)
-    if BufferApi.docBuf.buf == events.buf then
+    if BufferApi.__docbuf.buf == events.buf then
       vim.api.nvim_buf_delete(events.buf, { force = true, unload = false })
     end
   end,
@@ -15,13 +15,13 @@ vim.api.nvim_create_autocmd('BufLeave', {
 --Open a buffer in a new window
 --@param winName string: New window name
 --@param contents string[]: Array of lines to set buffer contents
-function BufferApi:docOpen(winName, contents)
+function BufferApi:opendoc(winName, contents)
   local buf = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_buf_set_name(buf, winName)
   vim.api.nvim_buf_set_lines(buf, 0, -1, true, contents)
   vim.api.nvim_set_option_value('readonly', true, { buf = buf })
   local win = vim.api.nvim_open_win(buf, true, { split = 'above', win = 0 })
-  BufferApi.docBuf = { buf = buf, win = win }
+  BufferApi.__docbuf = { buf = buf, win = win }
   Log.debug('BufferApi: docOpen: ' .. winName .. ' was opened. win: ' .. win .. ' buf: ' .. buf)
 end
 
